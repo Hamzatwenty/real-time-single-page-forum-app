b3adaac5939b935f0dc16aa9762c46feddfd62cf
MuhammadHamza <Hamzatwenty>
2020-03-21 13:04:30 +0500

    Vuetify, Vue Router and Initial Components

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-footer class="pa-3">Jers
+        <v-spacer></v-spacer>
+        <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+</template>
+<script>
+    export default {
+        name: "AppFooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
